1db8fe86471541250322e08e11e1ed052db284b5
benjamin-wss <benjamin.wss@gmail.com>
2015-05-27 01:46:12 +0800

    Created Baseline
    1. Renamed class to RedisDataProvider from Class1. Looks stupid if it were left alone.
    2. Added a simple GetAllKeyValuePair method.

---
+++ RedisJiggeryPokery/RedisJiggeryPokery/Contracts/IRedisDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RedisJiggeryPokery.Contracts
+{
+    public interface IRedisDataProvider<T>
+    {
+        string ConnectionString { get; set; }
+        int DatabaseIndex { get; set; }
+
+        IList<T> GetAllKeyValuePair(int dbIndex = 0);
+    }
+}
